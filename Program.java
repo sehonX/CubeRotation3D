3cf85ea39c2400d19f41281ce84660301ee7c7cf
Pawel Jaskula <pj.jaskula@gmail.com>
2017-05-13 15:13:34 +0200

    Program
    Add Program class with main

---
+++ Program.java
@@ -0,0 +1,13 @@
+package Skladanie;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame frame = new Frame();
+		frame.setDefaultCloseOperation(3);
+		frame.setTitle("Zadanie 1");
+		frame.setVisible(true);
+	}
+
+}
